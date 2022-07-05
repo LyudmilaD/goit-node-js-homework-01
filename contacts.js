2cbbfc4638f972953107def4fa4dce6a0a677bad
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-07-05 12:04:15 +0300

    add structure

---
+++ contacts.js
@@ -0,0 +1,40 @@
+// contacts.js
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+//  */
+// // TODO: задокументировать каждую функцию
+// function listContacts() {
+//   // ...твой код
+// }
+
+// function getContactById(contactId) {
+//   // ...твой кодcommand
+// }
+
+// function removeContact(contactId) {
+//   // ...твой код
+// }
+
+// function addContact(name, email, phone) {
+// ...твой код
+// }
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+function listContacts() {
+  //   // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой кодcommand
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}

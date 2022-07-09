0bebe91f508ebd3ca6decc21d3eb64f65e6d7a9b
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-07-10 00:53:31 +0300

    done all tasks

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # goit-nodejs-homework-01
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+https://monosnap.com/file/99Yc03gvl6T9jOZaj5N2qLVo8Ar9Ga
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://monosnap.com/file/oKvoXXWVSmU4zgQgddj608vlOaTUBy
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://monosnap.com/file/TdkBPoXc1BhuKY4vWgnpBWOE29n0MK
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://monosnap.com/file/mvGVgQejvI3EsYwMyWOWH2h6wvN1eb
--- index.js
+++ index.js
@@ -1,68 +1,25 @@
 const contacts = require("./contacts");
-// Пакет yargs
-// const argv = require("yargs").argv;
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       contacts.listContacts();
-//       break;
-
-//     case "get":
-//       contacts.getContactById(id);
-//       break;
-
-//     case "add":
-//       contacts.addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       contacts.removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
-
-//  Модуль commander
-const { Command } = require("commander");
-const program = new Command();
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-
-program.parse(process.argv);
-
-const argv = program.opts();
-
-// TODO: рефакторить
+const argv = require("yargs").argv;
 function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+	switch (action) {
+		case "list":
+			contacts.listContacts();
+			break;
 
-    case "get":
-      // ... id
-      break;
+		case "get":
+			contacts.getContactById(id);
+			break;
 
-    case "add":
-      // ... name email phone
-      break;
+		case "add":
+			contacts.addContact(name, email, phone);
+			break;
 
-    case "remove":
-      // ... id
-      break;
+		case "remove":
+			contacts.removeContact(id);
+			break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
 }
-
 invokeAction(argv);
